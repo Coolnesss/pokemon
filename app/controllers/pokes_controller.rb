b9eb946023f8e9ace6c5e7bdb920db4afb6020bf
Chang <chang.rajani@gmail.com>
2015-03-18 16:21:59 +0200

    Can now create new Pokemon

--- app/controllers/pokes_controller.rb
+++ app/controllers/pokes_controller.rb
@@ -8,4 +8,29 @@ class PokesController < ApplicationController
     @poke = Poke.find_by_id(params[:id])
   end
 
+  def new
+    @poke = Poke.new
+  end
+
+  def create
+    @poke = Poke.new(poke_params)
+
+    respond_to do |format|
+      if @poke.save
+        format.html { redirect_to @poke, notice: 'Pokemon was successfully created.' }
+        format.json { render :show, status: :created, location: @poke }
+      else
+        format.html { render :new }
+        format.json { render json: @poke.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  private
+
+    def poke_params
+      params.require(:poke).permit(:name)
+    end
+
 end
--- app/models/poke.rb
+++ app/models/poke.rb
@@ -1,5 +1,9 @@
 class Poke < ActiveRecord::Base
 
+  validates :name,
+    uniqueness: true,
+    length: { in: 3..11 } #Longest Pokemon name is Fletchinder
+
   def self.sqlAll
     Poke.find_by_sql "SELECT * FROM pokes"
   end
