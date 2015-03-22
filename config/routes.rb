799ead66753b4ff92348d4560200155a9d9c68f2
Chang <chang.rajani@gmail.com>
2015-03-22 18:59:19 +0200

    Added links for signout and signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'signin' => 'sessions#new'
   get 'signup' => 'users#new'
-
+  get 'signout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
