0c0dd55afa5231161b1299d37e5cd85d6dee9171
Chang <chang.rajani@gmail.com>
2015-03-23 03:05:59 +0200

    New listing for pokes

--- db/schema.rb
+++ db/schema.rb
@@ -16,17 +16,6 @@ ActiveRecord::Schema.define(version: 20150322115234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "account", primary_key: "user_id", force: :cascade do |t|
-    t.string   "username",   limit: 50,  null: false
-    t.string   "password",   limit: 50,  null: false
-    t.string   "email",      limit: 355, null: false
-    t.datetime "created_on",             null: false
-    t.datetime "last_login"
-  end
-
-  add_index "account", ["email"], name: "account_email_key", unique: true, using: :btree
-  add_index "account", ["username"], name: "account_username_key", unique: true, using: :btree
-
   create_table "pokes", primary_key: "poke_id", force: :cascade do |t|
     t.string "name", limit: 50, null: false
   end
