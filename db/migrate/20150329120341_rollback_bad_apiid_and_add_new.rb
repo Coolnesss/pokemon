eba644483f54f280bfc13ededae3e75199f702ba
Chang <chang.rajani@gmail.com>
2015-04-26 20:34:04 +0300

    Heroku migrate fix

--- db/migrate/20150329120341_rollback_bad_apiid_and_add_new.rb
+++ db/migrate/20150329120341_rollback_bad_apiid_and_add_new.rb
@@ -1,6 +1,5 @@
 class RollbackBadApiidAndAddNew < ActiveRecord::Migration
   def change
-    execute "ALTER TABLE user_pokes DROP COLUMN api_id"
     execute "ALTER TABLE pokes ADD COLUMN api_id INT"
   end
 end
